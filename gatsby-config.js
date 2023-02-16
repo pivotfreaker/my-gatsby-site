9a4d4b1d0f9dd7c353f8d61f576df80e1b668103
Peter <vrabie.petru@yahoo.ca>
2023-02-16 02:25:21 -0500

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,16 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
- module.exports = {
+module.exports = {
   siteMetadata: {
     title: "My First Gatsby Site",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
